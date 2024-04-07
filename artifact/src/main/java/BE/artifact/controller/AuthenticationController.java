9196a522d3c29c464c7e2b8b9e78271cf5d97cdf
Andrei Vulpe <andreivulpe02@yahoo.com>
2024-04-07 15:56:12 +0300

    Added PreAuthorize on signup endpoint

--- artifact/src/main/java/BE/artifact/controller/AuthenticationController.java
+++ artifact/src/main/java/BE/artifact/controller/AuthenticationController.java
@@ -4,8 +4,10 @@ import BE.artifact.payload.request.SignInRequest;
 import BE.artifact.payload.request.SignUpRequest;
 import BE.artifact.payload.response.JwtAuthenticationResponse;
 import BE.artifact.service.AuthenticationService;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
     private final AuthenticationService authenticationService;
+
     @PostMapping("/signup")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
         return ResponseEntity.ok(authenticationService.signup(request));
     }
