c2174f4e7c368ceee63eaddb19de26983217fdfa
Andrei Vulpe <andreivulpe02@yahoo.com>
2024-04-13 13:20:35 +0300

    PdfTemplate initial configuration

--- artifact/src/main/java/BE/artifact/controller/PdfController.java
+++ artifact/src/main/java/BE/artifact/controller/PdfController.java
@@ -19,25 +19,5 @@ public class PdfController {
     @Autowired
     private PdfFormFillerService pdfFormFillerService;
 
-    @GetMapping("/download-filled-form")
-    public ResponseEntity<byte[]> downloadFilledForm() {
-        try {
-            Map<String, String> formData = new HashMap<>();
-            formData.put("name", "John Doe");
-            formData.put("date", "2021-09-01");
-            byte[] pdfBytes = pdfFormFillerService.fillPdfTemplate("path/to/template.pdf", formData);
 
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_PDF);
-            String filename = "filled_form.pdf";
-            headers.setContentDispositionFormData("attachment", filename);
-            headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-
-            return ResponseEntity.ok()
-                    .headers(headers)
-                    .body(pdfBytes);
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
-    }
 }
